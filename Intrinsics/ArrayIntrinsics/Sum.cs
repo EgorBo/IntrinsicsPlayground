db7ed11196dc6c733a2e33c8a114cdff0d9021f1
EgorBo <egorbo@gmail.com>
2018-05-14 19:00:53 +0300

    optimize Sum

--- Intrinsics/ArrayIntrinsics/Sum.cs
+++ Intrinsics/ArrayIntrinsics/Sum.cs
@@ -14,39 +14,50 @@ namespace IntrinsicsPlayground
 
             const int vecSize = 8;
 
-            if (array.Length < 8)
-                return Sum_Soft(array, 0);
-            
-            int i = 0;
-            Vector256<float> sum = Avx.SetZeroVector256<float>();
             fixed (float* ptr = &array[0])
             {
-                for (; i <= array.Length - vecSize; i += vecSize)
+                if (array.Length < 8)
+                    return Sum_Soft(ptr, array.Length);
+
+                int i = 0;
+                Vector256<float> sum = Avx.SetZeroVector256<float>();
                 {
-                    var current = Avx.LoadVector256(ptr + i);
-                    sum = Avx.Add(current, sum);
+                    for (; i <= array.Length - vecSize; i += vecSize)
+                    {
+                        var current = Avx.LoadVector256(ptr + i);
+                        sum = Avx.Add(current, sum);
+                    }
                 }
+
+                // store __m256 into float[8] and sum all values
+                // as an alternative we can do (copied from https://stackoverflow.com/a/18616679/298088)
+                /*
+                    var t1 = Avx.HorizontalAdd(sum, sum);
+                    var t2 = Avx.HorizontalAdd(t1, t1);
+                    var t3 = Avx.ExtractVector128(t2, 1);
+                    var t4 = Sse.Add(Avx.GetLowerHalf(t2), t3);
+                    var hSum =  Sse.ConvertToSingle(t4);
+                 */
+
+                var result = stackalloc float[vecSize];
+                Avx.Store(result, sum);
+
+                float finalSum = *(result + 0) + *(result + 1) + *(result + 2) + *(result + 3)
+                               + *(result + 4) + *(result + 5) + *(result + 6) + *(result + 7);
+
+                if (i < array.Length)
+                    finalSum += Sum_Soft(ptr + i, array.Length - i);
+
+                return finalSum;
             }
-
-            // store __m256 into float[8] and sum all values
-            var result = stackalloc float[vecSize];
-            Avx.Store(result, sum);
-
-            float finalSum = *(result + 0) + *(result + 1) + *(result + 2) + *(result + 3)
-                           + *(result + 4) + *(result + 5) + *(result + 6) + *(result + 7);
-
-            if (i < array.Length - 1)
-                finalSum += Sum_Soft(array, i);
-
-            return finalSum;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static float Sum_Soft(float[] array, int offset)
+        private static float Sum_Soft(float* array, int count)
         {
             float sum = 0.0f;
-            for (; offset < array.Length; offset++)
-                sum += array[offset];
+            for (int i = 0; i < count; i++)
+                sum += *(array + i);
             return sum;
         }
     }
