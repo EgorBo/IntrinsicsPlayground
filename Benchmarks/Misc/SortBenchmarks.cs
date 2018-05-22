7aa86ab6815a40841993898c29db48e9c6ceb82f
EgorBo <egorbo@gmail.com>
2018-05-22 06:32:54 +0300

    add sort benchmark: Array.Sort vs DualPivotQuickSort (Java port)

---
+++ Benchmarks/Misc/SortBenchmarks.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using IntrinsicsPlayground.Misc;
+
+namespace IntrinsicsPlayground.Benchmarks.Misc
+{
+    //[MemoryDiagnoser]
+    public class SortBenchmarks
+    {
+        private int[] _alreadySortedAsc,
+            _alreadySortedDesc,
+            _nearlySortedAsc,
+            _fullRandom1,
+            _fullRandom2;
+
+        [Params(100, 100000)]
+        public int Count { get; set; }
+
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            var rand = new Random();
+            var range = Enumerable.Range(0, Count).ToArray();
+
+            _alreadySortedAsc = range.ToArray();
+            _alreadySortedDesc = range.Reverse().ToArray();
+
+            _nearlySortedAsc = _alreadySortedAsc.ToArray();
+            for (int i = 0; i < _nearlySortedAsc.Length - 1; i += rand.Next(5, 15))
+                _nearlySortedAsc[i] = 0;
+
+            _fullRandom1 = range.OrderBy(i => rand.Next()).ToArray();
+            _fullRandom2 = range.OrderBy(i => rand.Next()).ToArray();
+        }
+
+
+        [Benchmark]
+        public int[] AlreadySortedAsc_Java()
+        {
+            var array = _alreadySortedAsc.ToArray();
+            DualPivotQuicksort.Sort(array);
+            return array;
+        }
+
+        [Benchmark]
+        public int[] AlreadySortedAsc__NET()
+        {
+            var array = _alreadySortedAsc.ToArray();
+            Array.Sort(array);
+            return array;
+        }
+
+
+        [Benchmark]
+        public int[] AlreadySortedButReversed_Java()
+        {
+            var array = _alreadySortedDesc.ToArray();
+            DualPivotQuicksort.Sort(array);
+            return array;
+        }
+
+        [Benchmark]
+        public int[] AlreadySortedButReversed__NET()
+        {
+            var array = _alreadySortedDesc.ToArray();
+            Array.Sort(array);
+            return array;
+        }
+
+
+        [Benchmark]
+        public int[] NearlySortedAsc_Java()
+        {
+            var array = _nearlySortedAsc.ToArray();
+            DualPivotQuicksort.Sort(array);
+            return array;
+        }
+
+        [Benchmark]
+        public int[] NearlySortedAsc__NET()
+        {
+            var array = _nearlySortedAsc.ToArray();
+            Array.Sort(array);
+            return array;
+        }
+
+
+        [Benchmark]
+        public int[] FullRandom1_Java()
+        {
+            var array = _fullRandom1.ToArray();
+            DualPivotQuicksort.Sort(array);
+            return array;
+        }
+
+        [Benchmark]
+        public int[] FullRandom1__NET()
+        {
+            var array = _fullRandom1.ToArray();
+            Array.Sort(array);
+            return array;
+        }
+
+
+        [Benchmark]
+        public int[] FullRandom2_Java()
+        {
+            var array = _fullRandom2.ToArray();
+            DualPivotQuicksort.Sort(array);
+            return array;
+        }
+
+        [Benchmark]
+        public int[] FullRandom2__NET()
+        {
+            var array = _fullRandom2.ToArray();
+            Array.Sort(array);
+            return array;
+        }
+    }
+}
---
+++ Tests/SortTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using IntrinsicsPlayground.Misc;
+using Xunit;
+
+namespace IntrinsicsPlayground.Tests
+{
+    public class SortTests
+    {
+        [Fact]
+        public void DualPivotQuicksortTest()
+        {
+            var rand = new Random();
+            var array1 = Enumerable.Range(0, 1024).OrderBy(i => rand.Next()).ToArray();
+            var array2 = array1.ToArray();
+
+            Array.Sort(array1);
+            DualPivotQuicksort.Sort(array2);
+
+            Assert.True(array1.SequenceEqual(array2));
+        }
+    }
+}
